73c9429abdfb8cd5987cd19a40893a26204684cc
developerSaquib <developer.saquib97@gmail.com>
2024-12-29 20:16:01 +0530

    constant updated

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:3000/api/v1';
 
-export const BASE_URL = "https://backend-jhstudio.vercel.app/";
+export const BASE_URL = "https://backend-jhstudio.vercel.app/api/v1/";
